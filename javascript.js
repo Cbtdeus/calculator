acea0385462428979c96e8643d09a7e81fddb526
Cbtdeus <cbtdeus@proton.me>
2024-09-08 23:32:18 +0300

    ui refactor

--- javascript.js
+++ javascript.js
@@ -36,111 +36,130 @@ function operate(x,y,operator){
 }
 const calcDiv = document.querySelector("div")
 const textbox = document.createElement("input")
+textbox.disabled = true
 const keypad = document.createElement("div")
 
 
 const clearbutton = document.createElement("button")
-clearbutton.textContent = "clear"
+clearbutton.textContent = "AC"
 clearbutton.setAttribute("class", "key")
 clearbutton.addEventListener("click", () => {
     num1 = 0
     operator = ""
    textbox.value = ""
 })
-keypad.appendChild(clearbutton)
 
 
 keypad.setAttribute("class", "keypad")
 calcDiv.appendChild(textbox)
 textbox.setAttribute("class", "textbox")
 calcDiv.appendChild(keypad)
-for (let i = 0 ; i !== 10; i++){
-
-    const button = document.createElement("button")
-    button.setAttribute("class", "number")
-    button.setAttribute("class", "key")
-    button.textContent = i
-    button.addEventListener("click", () => textbox.value = String(textbox.value) + i)
-    keypad.appendChild(button)
-} 
 
 
 let operator = ""
 const dividebutton = document.createElement("button")
-dividebutton.setAttribute("class", "key")
+dividebutton.classList.add("operator","key")
 dividebutton.addEventListener("click", () => {operator = "/"
     num1 = Number(textbox.value)
 textbox.value = ""
 })
 dividebutton.textContent = "/"
-keypad.appendChild(dividebutton)
+
 
 
 const additionbutton = document.createElement("button")
-additionbutton.setAttribute("class", "key")
+additionbutton.classList.add("operator","key")
 additionbutton.textContent = "+"
 additionbutton.addEventListener("click", () => {operator = "+"
     num1 = Number(textbox.value)
     textbox.value = ""
     })
-keypad.appendChild(additionbutton)
+
    
 
 const subractionbutton = document.createElement("button")
-subractionbutton.setAttribute("class", "key")
+subractionbutton.classList.add("operator","key")
 subractionbutton.textContent = "-"
 subractionbutton.addEventListener("click", () => {operator = "-"
     num1 = Number(textbox.value)
     textbox.value = ""
     })
-keypad.appendChild(subractionbutton)
+
 
 
 const multiplybutton = document.createElement("button")
-multiplybutton.setAttribute("class", "key")
+multiplybutton.classList.add("operator","key")
 multiplybutton.addEventListener("click", () => {operator = "*"
     num1 = Number(textbox.value)
     textbox.value = ""
 })
 multiplybutton.textContent = "*"
-keypad.appendChild(multiplybutton)    
+ 
 
 
 const equalbutton = document.createElement("button")
-equalbutton.setAttribute("class", "key")
+equalbutton.classList.add("operator","key")
 equalbutton.setAttribute("id", "equal")
 equalbutton.textContent = "="
 equalbutton.addEventListener("click", () => {
     num2 = Number(textbox.value)
    textbox.value = operate(num1, num2, operator)
 })
-keypad.appendChild(equalbutton)
+
 
 const removelastbutton = document.createElement("button")
-removelastbutton.setAttribute("class", "key")
+removelastbutton.classList.add("utility","key")
 removelastbutton.textContent = "C"
 removelastbutton.addEventListener("click", () =>{
     textbox.value = textbox.value.substring(0, textbox.value.length - 1)
 })
-keypad.appendChild(removelastbutton)
 
 
-const negativeToggle = document.createElement("button")                                                             
-negativeToggle.setAttribute("class", "key")
-negativeToggle.textContent = "±"                                                                              /* TODO: make selected operator button glow */
-negativeToggle.addEventListener("click", () =>{
+
+const negativeButton = document.createElement("button")                                                             
+negativeButton.classList.add("utility","key")
+negativeButton.textContent = "±"                                                                              /* TODO: make selected operator button glow */
+negativeButton.addEventListener("click", () =>{
     if (textbox.value.charAt(0) == "-"){
-        textbox.value = textbox.value.slice(1)                                                                  /* TODO: remove user access to textbox */
+        textbox.value = textbox.value.slice(1)                                                                  /* TODO: remove user access to textbox and add keyboard support thru listeners */
     }
     else{
         textbox.value = "-" + textbox.value
-    }
+    }                                                                                                                  
 })
-keypad.appendChild(negativeToggle)
+
 
 const decimalButton = document.createElement("button")
+decimalButton.classList.add("utility","key")
 decimalButton.textContent = "."
 decimalButton.addEventListener("click", () => {
-    textbox.textContent = textbox.textContent + "."
+    if (textbox.value.includes(".")){
+        return
+    }                                                                                                                                  
+    textbox.value = textbox.value + "."
+
 })
+
+keypad.appendChild(dividebutton)
+keypad.appendChild(negativeButton)
+keypad.appendChild(clearbutton)
+keypad.appendChild(removelastbutton)
+ 
+for (let i = 0 ; i !== 10; i++){
+
+    const button = document.createElement("button")
+    button.classList.add("number","key")
+    button.setAttribute("id", i)
+    button.textContent = i
+    button.addEventListener("click", () => textbox.value = String(textbox.value) + i)
+    keypad.appendChild(button)
+} 
+
+keypad.insertBefore(multiplybutton, document.getElementById("4"))
+keypad.insertBefore(subractionbutton, document.getElementById("7"))
+keypad.appendChild(additionbutton)
 keypad.appendChild(decimalButton)
+
+keypad.appendChild(equalbutton)
+keypad.insertBefore(document.getElementById("0"), equalbutton)
+
